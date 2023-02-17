c5e5678c9d44cc963a61126aa5d405f558b8001d
NithishQuinnox <123157110+NithishQuinnox@users.noreply.github.com>
2023-02-17 22:44:46 +0530

    Delete src directory

--- src/test/java/PageFactory/CustomerConstants.java
+++
@@ -1,7 +0,0 @@
-package PageFactory;
-
-public class CustomerConstants {
-
-	public static final String addCustomer="Add Customer";
-	public static final String addCustomerAlert="please fill all fields";
-}
