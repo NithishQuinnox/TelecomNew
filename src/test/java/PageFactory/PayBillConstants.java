cc7f06a454689139985ac2f04cb2f2664ff01b7a
NithishQuinnox <123157110+NithishQuinnox@users.noreply.github.com>
2023-02-17 22:14:18 +0530

    Delete src directory

--- src/test/java/PageFactory/PayBillConstants.java
+++
@@ -1,8 +0,0 @@
-package PageFactory;
-
-public class PayBillConstants {
-
-	public static final String payBill_Heading="Pay Billing";
-	public static final String enterBill_CustId_Label="Enter Your Customer ID";
-	public static final String addNoBill_Alert_Msg="Please Correct Value Input";
-}
